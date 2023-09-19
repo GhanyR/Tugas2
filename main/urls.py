540dc780886d6e93ab6fc3308f91328cdd2ecc6b
GhanyR <neoperkasa@gmail.com>
2023-09-20 01:08:41 +0700

    git push

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('remove_item/<int:pk>/', remove_item, name='remove_item'),
     path('', views.all_items_detail, name='all_items_detail'),
     path('item_detail/<int:pk>/', views.item_detail, name='item_detail'),
+    path('view_html/', views.view_html, name='view_html'),
+    path('view_xml/', views.view_xml, name='view_xml'),
+    path('view_json/', views.view_json, name='view_json'),
+    path('view_xml_id/<int:pk>/', views.view_xml_id, name='view_xml_id'),
+    path('view_json_id/<int:pk>/', views.view_json_id, name='view_json_id'),
 ]
     
--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Item
 from .forms import ItemForm
+from django.http import JsonResponse, HttpResponse
+from django.core import serializers
+from django.template import loader
 
 def item_detail(request, pk):
     item = Item.objects.get(pk=pk)
@@ -33,6 +36,30 @@ def all_items_detail(request):
         'items': items, 
         'name': 'Ghany Rasyid Prawira',
         'class': 'PBP C',
-        'npm': '2206082392'
+        'npm': '2206082392',
+        'item_count': items.count(),
     }
     return render(request, 'main/all_items_detail.html', context)
+
+def view_html(request):
+    items = Item.objects.all()
+    template = loader.get_template('main/view_html.html')
+    context = {'items': items}
+    return HttpResponse(template.render(context, request))
+
+def view_xml(request):
+    items = serializers.serialize('xml', Item.objects.all())
+    return HttpResponse(items, content_type='application/xml')
+
+def view_json(request):
+    items = serializers.serialize('json', Item.objects.all())
+    return JsonResponse(items, safe=False)
+
+def view_xml_id(request, pk):
+    item = serializers.serialize('xml', [Item.objects.get(pk=pk)])
+    return HttpResponse(item, content_type='application/xml')
+
+def view_json_id(request, pk):
+    item = serializers.serialize('json', [Item.objects.get(pk=pk)])
+    return JsonResponse(item, safe=False)
+
